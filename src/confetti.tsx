f8a31f0e9c027793d502d730941107b6d2657195
Rocco Maniscalco <rmaniscalco@magmutual.com>
2024-03-19 11:36:14 -0400

    remove redundant code

--- src/confetti.tsx
+++ src/confetti.tsx
@@ -4,10 +4,6 @@ import { onMount } from "solid-js";
 export default function Confetti() {
   onMount(() =>
     confetti({
-      origin: {
-        y: 0.5,
-        x: 0.5,
-      },
       spread: 360,
       startVelocity: 30,
     })
