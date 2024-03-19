c6e5bfc3e976ae3d3c2a6b7e96b9a98fcd746751
Rocco Maniscalco <rmaniscalco@magmutual.com>
2024-03-19 19:18:44 -0400

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
 import db from "@astrojs/db";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), tailwind(), solidJs()],
   output: "server",
+  adapter: vercel()
 });
