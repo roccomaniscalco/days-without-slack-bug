33d35fd393d0e52370acb6deb30be712498e3046
Rocco Maniscalco <rmaniscalco@magmutual.com>
2024-03-20 00:22:55 -0400

    icons + color revamp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import db from "@astrojs/db";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind(), solidJs()],
+  integrations: [
+    db(),
+    tailwind(),
+    solidJs(),
+    icon({ include: { heroicons: ["trophy-16-solid", "calendar-days-16-solid"] } }),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
--- src/components/reset-button.tsx
+++ src/components/reset-button.tsx
@@ -8,7 +8,7 @@ export default function ResetButton() {
 
   return (
     <button
-      class="bg-gray-700/70 rounded-md font-semibold hover:bg-gray-700/60 active:bg-gray-700/50 focus:ring-2 ring-purple-400 ring-offset-2 ring-offset-slate-900 outline-none px-3 py-1"
+      class="bg-slate-700/70 rounded-md font-semibold hover:bg-slate-700/60 active:bg-slate-700/50 focus:ring-2 ring-purple-400 ring-offset-2 ring-offset-slate-900 outline-none px-3 py-1"
       onClick={resetDaysSinceBug}
     >
       Reset
